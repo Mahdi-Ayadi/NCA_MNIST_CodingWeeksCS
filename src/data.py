645f5635a4c27f43977ff2a485fbb860b84a866b
Blaiech Youssef <youssef.blaiech@student-cs.fr>
2024-11-19 18:21:09 +0100

    training loop

--- src/data.py
+++ src/data.py
@@ -14,15 +14,6 @@ if not (os.path.exists(path)):
     # Download the dataset
     dataloader = torchvision.datasets.MNIST(root="", download=True, transform=torchvision.transforms.ToTensor())
 
-    # Create empty numpy arrays to store the data: X_dataset are the images and Y_dataset are the labels
-    X_dataset = np.empty((len(dataloader), 28, 28))
-    Y_dataset = np.empty((len(dataloader), 1))
-
-    # Fill the numpy arrays with the data
-    for i, (image, label) in enumerate(dataloader):
-        X_dataset[i] = image.numpy().squeeze()
-        Y_dataset[i] = label
-
 else:
     transform = transforms.ToTensor()
     mnist_dataset = datasets.MNIST(root="./data", train=True, download=True, transform=transform)
@@ -31,3 +22,4 @@ else:
     plt.imshow(image.squeeze(), cmap='gray')
     plt.show()
     print(f"Label: {label}")
+    
