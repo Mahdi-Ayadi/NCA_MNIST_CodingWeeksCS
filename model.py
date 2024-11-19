2004586e039a98895b257dd4dd362faf7ebc64d8
Soufargi Rayen <rayen.soufargi@centralesupelec.fr>
2024-11-19 10:53:58 +0100

    ajout de _c

--- model.py
+++ model.py
@@ -66,7 +66,7 @@ class NCA(nn.Module):
             mask = mask.unsqueeze(-1).expand_as(delta_grid)  # Shape: (batch_size, height, width, n_channels)
 
             # Aplly the mask
-            delta_grid = delta_grid * mask           
+            delta_grid_c = delta_grid_c * mask           
 
             #add the delta grid to the grid
             grid = grid + delta_grid_c
