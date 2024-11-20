0586d98c2a08283fd898325e4bcdeafc8a8ad4da
Ayadi Mahdi <mahdi.ayadi@student-cs.fr>
2024-11-20 19:01:23 +0100

    Added plots of accuracy in results folder

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
  
 #    Sprint 0:
+
 F1: installation des modules nécessaitres en local: Pytorch, Matplotlib, tkinter 
 
 F2: création du repo git et version control
@@ -11,6 +12,7 @@ F2: création du repo git et version control
 F3: conception globale
 
 #   Sprint 1: Network Training
+
 F4: récupération & encodage du Dataset
 
 F5: Définition du modèle NCA (POO)
@@ -22,16 +24,25 @@ F7: validation du modèle
 F8: sauvegarde des poids du modèle
 
 #    Sprint 2: Visualisation et intégration du modèle 
+
 F9: implémentation avec matplotlib.animation
 
 F10: création de la fonction main (input ==> identification par code de couleurs)
 
 #    Sprint 3: développement de l'UI:
+
 F11: Préparation de l'interface TKINTER
 
 F12: Visualisation des grilles en évolution sur TKINTER
 
 F13: création de l'outil du dessin autonome
+
+F14: Amélioration de la solution en utilisant Pygame au lieu de Tkinter 
+
+#  Sprint 4: Améliorations du projet
+
+F15: Ajout des courbes d'évolution de la précision (précision générale, précision sur chaque chiffre) et des courbes de loss lors du training
+
 
 #  Conception
 
--- src/model_validation.py
+++ src/model_validation.py
@@ -3,12 +3,28 @@ import numpy as np
 from torchvision import datasets, transforms
 from model import *
 import matplotlib.pyplot as plt
+from torch.utils.data import Subset
 
+# Import the MNIST dataset
 transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Lambda(lambda x: x.squeeze(0))
     ])
 test_dataset = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+
+# Separate the dataset into ten datasets
+digit_datasets = {digit: [] for digit in range(10)}  # Dictionary to hold datasets for each digit
+
+for idx, (image, label) in enumerate(test_dataset):
+    digit_datasets[label].append(idx)
+
+# Convert each list of indices into a Subset
+digit_subsets = {digit: Subset(test_dataset, indices) for digit, indices in digit_datasets.items()}
+
+# Create DataLoaders for each digit
+digit_loaders = {digit: DataLoader(subset, batch_size=1, shuffle=True) for digit, subset in digit_subsets.items()}
+
+# Create a DataLoader for the whole test dataset (in case we want to calculate the accuracy for the whole dataset without separating numbers)
 test_loader = DataLoader(test_dataset,batch_size=1, shuffle=True, pin_memory=True)
 
 def accuracy_per_image(input,label,n_steps):
@@ -29,37 +45,62 @@ def accuracy_per_image(input,label,n_steps):
     
     return percentages
             
-def accuracy(test_loader, n_steps, model):
-    N = len(test_loader)
+def accuracy(dataset, n_steps, model):
+    N = len(dataset)
     accuracy_table = np.zeros((n_steps), dtype=np.float64)
     percentages_table = np.zeros((N, n_steps), dtype=np.float64)
-    for i, (input, label) in tqdm(enumerate(test_loader), total=len(test_loader), desc="Processing"):
+    for i, (input, label) in tqdm(enumerate(dataset), total=len(dataset), desc="Processing"):
         percentages_table[i] = accuracy_per_image(input.squeeze(0), label.squeeze(0) ,n_steps)
     accuracy_table = np.mean(percentages_table,axis=0)
     return accuracy_table
     
 if __name__ == "__main__":
+    # To choose to calculate the accuracy for each number independently or for the entire dataset as a whole
+    accuracy_per_number = True
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.load('model_full.pth', map_location=device)
     model = model.to(device)  # Ensure the model is on the correct device
     model.eval()
+    
     # Compute the accuracy array
-    n_steps = 100  # Example number of steps
-    accuracy_array = accuracy(test_loader, n_steps, model)
+    n_steps = 300
+    if accuracy_per_number: 
+        for i in range(10):
+            dataset = digit_loaders[i]
+            accuracy_array = accuracy(dataset, n_steps, model)
+            plt.figure(figsize=(8, 6))
+            plt.plot(range(n_steps), accuracy_array, marker='o', linestyle='-', color='b', label='Accuracy')
+            plt.title(f'Accuracy vs Steps for number {i}', fontsize=16)
+            plt.xlabel('Steps', fontsize=14)
+            plt.ylabel('Accuracy', fontsize=14)
+            plt.grid(True)
+            plt.legend(fontsize=12)
+            plt.tight_layout()
+            
+            # Save the plot
+            output_path = f"accuracy_vs_steps_number_{i}.png"
+            plt.savefig(output_path)
+            plt.show()
+            print(f"Plot saved as {output_path}")
+    
+    else:
+        dataset = test_loader
+        accuracy_array = accuracy(dataset, n_steps, model)
 
-    # Plot the accuracy vs. steps
-    plt.figure(figsize=(8, 6))
-    plt.plot(range(n_steps), accuracy_array, marker='o', linestyle='-', color='b', label='Accuracy')
-    plt.title('Accuracy vs Steps', fontsize=16)
-    plt.xlabel('Steps', fontsize=14)
-    plt.ylabel('Accuracy', fontsize=14)
-    plt.grid(True)
-    plt.legend(fontsize=12)
-    plt.tight_layout()
+        plt.figure(figsize=(8, 6))
+        plt.plot(range(n_steps), accuracy_array, marker='o', linestyle='-', color='b', label='Accuracy')
+        plt.title('Accuracy vs Steps', fontsize=16)
+        plt.xlabel('Steps', fontsize=14)
+        plt.ylabel('Accuracy', fontsize=14)
+        plt.grid(True)
+        plt.legend(fontsize=12)
+        plt.tight_layout()
 
-    # Save the plot
-    output_path = "accuracy_vs_steps.png"
-    plt.savefig(output_path)
-    plt.show()
+        # Save the plot
+        output_path = "accuracy_vs_steps.png"
+        plt.savefig(output_path)
+        plt.show()
 
-    print(f"Plot saved as {output_path}")
+        print(f"Plot saved as {output_path}")
     
